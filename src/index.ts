6223205fc908cd157d9d04a908370b1c70d5cb68
zekunjin <zekun.jin@foxmail.com>
2024-01-08 11:24:27 +0800

    chore:  use base cron job

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-const main = () => {
+import { CronJob } from 'cron'
+import { loadConfig } from 'c12'
+import zerossl from './zerossl'
+
+const run = <T extends (config: CertgenConfig) => void>(cmd: T) => () => {
+  loadConfig<CertgenConfig>({ name: 'certgen' }).then(({ config }) => { if (config) cmd(config) })
   
 }
+
+new CronJob('* * * * * *', run(zerossl), null, true, 'America/Los_Angeles')

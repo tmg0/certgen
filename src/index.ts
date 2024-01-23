81c9696fec6d79036cb49d7469aec5e952812d49
zekunjin <zekun.jin@foxmail.com>
2024-01-23 14:31:29 +0800

    chore: use jiti

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,12 @@ import { CronJob } from 'cron'
 import { loadConfig } from 'c12'
 import zerossl from './zerossl'
 
+const main = () => run(zerossl)
+
 const run = <T extends (config: CertgenConfig) => void>(cmd: T) => () => {
   loadConfig<CertgenConfig>({ name: 'certgen' }).then(({ config }) => { if (config) cmd(config) })
 }
 
-new CronJob('* * * * * *', run(zerossl))
+new CronJob('* * * * * *', main())
+
+main()
